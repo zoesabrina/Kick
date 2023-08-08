c98ef77dafcce76c0ae3fb214e0733ccce7f19f0
Sabrina Zoe <96883466+zoesabrina@users.noreply.github.com>
2023-08-07 22:16:14 -0300

    desafio paradas 42-43

---
+++ Desafio 42-43.py
@@ -0,0 +1,55 @@
+# Exercício 1:
+
+val1 = float(input("Digite um valor: "))
+val2 = float(input("Digite um segundo valor: "))
+
+resultado = val1 + val2
+
+print("O resultado da soma é: ", resultado)
+
+
+
+# Exercício 2:
+"""
+nome = input("Informe seu nome: ")
+idade = int(input("informe sua idade: "))
+telefone = input("Informe seu telefone: ")
+email = input("informe seu e-mail: ")
+
+print("\nNome informado: ", nome, "\nIdade informada: ", idade, "\nTelefone informado: ", telefone, "\nE-mail informado: ", email,)
+
+"""
+
+# Exercício 3:
+"""
+codigo = input("Digite o código do produto: ")
+nome = input("Informe o nome do produto: ")
+quantidade = int(input("Digite a quantidade: "))
+preco = float(input("Informe o preço unitário do produto: "))
+
+valor_total = quantidade * preco
+
+print("\nCódigo:", codigo, "\nNome:", nome, "\nQuantidade:", quantidade, "\nPreço Unitário: R$", preco, "\nValor Total da Compra: R$", valor_total)
+
+"""
+
+# Exercício 4:
+"""
+cm = int(input("Informe um valor em centímetros para transformar em metros: "))
+
+m = cm / 100;
+
+print("o valor em centímetros: ",cm, "em metros fica: ", m)
+
+"""
+
+# Exercício 5:
+"""
+altura = float(input("Digite a altura do quadrado/retângulo: "))
+base = float(input("Digite a base do quadrado/retângulo: "))
+
+area = base * altura
+
+print("A área do quadrado/retângulo é:", area)
+
+"""
